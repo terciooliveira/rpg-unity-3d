d652048d8b6dcde4d868fa5241512a6d123e5dca
Tercio Almeida <tercio.oliveira.de.almeida@gmail.com>
2018-10-30 20:05:25 -0300

    Camera travada e seguindo

---
+++ Dragon RPG/Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = player.gameObject.transform.position;
+    }
+}
